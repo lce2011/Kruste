a8ab7d32078f9c5cfd55b59179c0aca55c2841a1
lce2011 <edelmannleo@gmail.com>
2026-09-04 20:40:11 +0200

    Added binary renames to build-scripts

--- scripts/full-build.bat
+++ scripts/full-build.bat
@@ -18,6 +18,7 @@ if %errorlevel% neq 0 (
 echo [====== CROSS BUILDS (cargo-cross) ======]
 echo Building for x86_64-unknown-linux-gnu (glibc)...
 cross build --release --target x86_64-unknown-linux-gnu
+mv ../target/x86_64-unknown-linux-gnu/release/kruste ../target/x86_64-unknown-linux-gnu/release/x86_64-glibc-kruste
 if %errorlevel% neq 0 (
     echo Build failed.
     exit /b 1
@@ -25,6 +26,7 @@ if %errorlevel% neq 0 (
 
 echo Building for x86_64-unknown-linux-musl (musl-libc)...
 cross build --release --target x86_64-unknown-linux-musl
+mv ../target/x86_64-unknown-linux-musl/release/kruste ../target/x86_64-unknown-linux-musl/release/x86_64-musl-kruste
 if %errorlevel% neq 0 (
     echo Build failed.
     exit /b 1
@@ -32,6 +34,7 @@ if %errorlevel% neq 0 (
 
 echo Building for aarch64-linux-android (Android BIONIC)...
 cross build --release --target aarch64-linux-android
+mv ../target/aarch64-linux-android/release/kruste ../target/aarch64-linux-android/release/aarch64-android-bionic-kruste
 if %errorlevel% neq 0 (
     echo Build failed.
     exit /b 1
